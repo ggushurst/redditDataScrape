b075437e13d8d916742bc561ac1e94ee42d3a510
Greg Gushurst <ggushurst@gmail.com>
2019-12-12 13:16:34 -0600

    Complete project! Scrapes the reddit website, then returns a json file with "User, Comments, Votes, Title".

--- src/main/java/URLReaderJava/App.java
+++ src/main/java/URLReaderJava/App.java
@@ -2,10 +2,17 @@ package URLReaderJava;
 
 //import java.net.*;
 import java.io.*;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,36 +27,34 @@ import org.json.simple.JSONObject;
 import java.io.FileWriter;
 import com.google.gson.*;
 
-public class App {
-    public static void ListsToMap(List<String> Titles, List<String> Votes) {
-        Map<String, String> Info = new LinkedHashMap<String, String>();
 
-        for (int i=0; i<Titles.size(); i++) {
-            Info.put(Titles.get(i), Votes.get(i));
-            System.out.println(Titles.get(i) + ":" + Votes.get(i));
-        }
-        System.out.println(Info);
-    }
+public class App {
+
     public static void main(String[] args) throws IOException {
+
+        //Connecting to the website
         Document doc = Jsoup.connect("https://old.reddit.com/").get();
-        System.out.println(doc.title());
+        System.out.println(doc.title()); //Print title of the website, for confirmation
+
+        //Creating the css query paths for various elements
         Elements content = doc.select("#siteTable div div.top-matter p.title a.title");
         Elements votes = doc.select("#siteTable div div.score.unvoted");
         Elements comments = doc.select("#siteTable div div ul");
         Elements userName = doc.select("#siteTable div div p.tagline a.author");
+
+        //Initializing the arrays that characteristic of the posts will be stored in
         ArrayList<String> postTitles = new ArrayList<String>();
-        ArrayList<String> postLinks = new ArrayList<String>();
         ArrayList<Integer> postVotes = new ArrayList<Integer>();
         ArrayList<Integer> postComments = new ArrayList<Integer>();
         ArrayList<String> postUserNames = new ArrayList<String>();
-        String case1 = "sponsored-indicator rank";
+
+        //Populating the arrays
         for (Element e: content) {
             postTitles.add(e.text());
-            postLinks.add(e.absUrl("href"));
         }
         for (Element v: votes) {
             String votesTemp = v.attr("title");
-            if(votesTemp.equals("")){
+            if(votesTemp.equals("")){ //If there are no comments for the posts, default will be 0
                 votesTemp = "0";
             }
             int votesFinal = Integer.parseInt((votesTemp));
@@ -58,50 +63,37 @@ public class App {
         for (Element c: comments) {
             String promoted = c.select("li span.promoted-span").text();
             String text = c.select("li.first a").text();
-            text = text.replaceAll("\\D+", "");
-//            System.out.println(promoted + " & " +text);
+            text = text.replaceAll("\\D+", ""); //Removing the letters and leaving only integers
             int number;
-            if(promoted.equals("promoted")){
-//                System.out.println("NAY");
+            if(promoted.equals("promoted")){ //Checking if posts is promotion, so it can be filtered out in postgres
                 number = -1;
             }
             else {
-//              System.out.println("YAY");
                 number = Integer.parseInt(text);
             }
             postComments.add(number);
         }
-//        System.out.println(postComments);
         for (Element u: userName) {
             postUserNames.add(u.text());
         }
 
-        //Creating a complete array of information
-        System.out.println("Titles " + postTitles.size() + " Links " + postLinks.size() +
-                " Votes " + postVotes.size() + " Comments " + postComments.size() +
-                " UserNames " + postUserNames.size());
-
-
-
-//        System.out.println(Data);
-
-
         //Creating a JSON file that will be read into database
-        JSONObject obj = new JSONObject();
-        JSONArray dlist = new JSONArray();
+
+        JSONObject masterObj = new JSONObject(); //Master object that will be used to write the json file
+        JSONArray header = new JSONArray();
         JSONObject data = new JSONObject();
-        //Data
-//        data.put("path", redditDataPath);
-//        data.put("lastprofile", profileList.getSelectedValue());
-        dlist.add(data);
-
-        obj.put("data", dlist);
 
 
+        //Insert header
+        data.put("Website", "old.reddit.com");
+        header.add(data);
+
+        //Insert header into master object
+        masterObj.put("data", header);
 
 
+        //Initializing JSON objects/arrays
         JSONObject postData = new JSONObject();
-        JSONObject postDataListObj = new JSONObject();
         JSONArray postDataList = new JSONArray();
 
         for(int i = 0; i <= postTitles.size() - 1; i++){
@@ -111,21 +103,18 @@ public class App {
             postData.put("Votes", postVotes.get(i));
             postData.put("Comments", postComments.get(i));
             postData.put("Username", postUserNames.get(i));
-            postData.put("Link", postLinks.get(i));
             postDataList.add(postData);
-            postDataListObj.put("post"+i, postDataList);
+
 
         }
-        obj.put("posts", postDataList);
+        masterObj.put("posts", postDataList);
 
 
-
-//        String redditData = new Gson().toJson(Data);
-//        System.out.println(redditData);
-
-//        Write json file
-        try (FileWriter file = new FileWriter("redditData.json")){
-            file.write(obj.toJSONString());
+        //Write json file
+        String time = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss")
+                .format(new java.util.Date()); //Created a timestamp to add to filename to have unique files
+        try (FileWriter file = new FileWriter("redditData_" + time + ".json")){
+            file.write(masterObj.toJSONString());
             file.flush();
         }
         catch (IOException e){
